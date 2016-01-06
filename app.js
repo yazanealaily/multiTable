7d266c16251bfc45308e1e715c97a6160e81b63d
yazanealaily <yazane.alaily@gmail.com>
2016-01-06 20:20:16 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+.controller('MultiplicationCtrl', function($scope, $attrs) {
+    $scope.numberLimit = $attrs.initialNumberLimit || 10;
+
+    $scope.numbers = function() {
+        var numbers = [];
+        for(var i=0; i<$scope.numberLimit; i++) {
+            numbers[i] = i + 1;
+        };
+        return numbers;
+    };
+
+    $scope.compute = function(a,b) {
+        return a * b;
+    };
+});
